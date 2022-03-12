a9ab327a0fbe81d061cff16d542c395721a368fc
prytula-illia <pritularllya@ukr.net>
2022-03-12 21:53:02 +0200

    Remove not used usings

--- BLL/Services/LearnModuleService.cs
+++ BLL/Services/LearnModuleService.cs
@@ -3,11 +3,7 @@ using BLL.DTOs;
 using BLL.Interfaces;
 using DAL.Entities;
 using DAL.Interfaces;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL.Services
 {
